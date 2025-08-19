92e4d2d3141002a5e9cc91f9fd5cb8b7db0ae619
Neil Earlam <neil.earlam123@icloud.com>
2025-08-19 21:42:40 +0100

    Added testing projects

---
+++ HotelBooker.CommonTests/TestingCategories.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelBooker.CommonTests
+{
+    public static class TestingCategories
+    {
+        public const string Unit = "Unit";
+        public const string Repository = "Repository";
+        public const string Integration = "Integration";
+    }
+}
