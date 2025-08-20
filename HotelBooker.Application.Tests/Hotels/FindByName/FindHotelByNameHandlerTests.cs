60cf676cb9720dbdbea5013eaf17641735dce464
Neil Earlam <neil.earlam123@icloud.com>
2025-08-20 15:34:37 +0100

    wrote tests for the FindHotelByNameHandler

---
+++ HotelBooker.Application.Tests/Hotels/FindByName/FindHotelByNameHandlerTests.cs
@@ -0,0 +1,73 @@
+﻿using HotelBooker.Application.Hotels.FindByName;
+using HotelBooker.Application.Interfaces.Repositories;
+using HotelBooker.Application.Models.Common;
+using HotelBooker.CommonTests;
+using HotelBooker.CommonTests.Fakers;
+using HotelBooker.Domain.Models;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using NSubstitute.ClearExtensions;
+using Shouldly;
+
+#nullable disable
+namespace HotelBooker.Application.Tests.Hotels.FindByName
+{
+    [TestFixture]
+    [Category(TestingCategories.Unit)]
+    public class FindHotelByNameHandlerTests
+    {
+        private FindHotelByNameHandler _handler;
+        private IHotelRepository _mockHotelRepository;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _mockHotelRepository = Substitute.For<IHotelRepository>();
+            _handler = new FindHotelByNameHandler
+            (
+                _mockHotelRepository,
+                Substitute.For<ILogger<FindHotelByNameHandler>>()
+            );
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockHotelRepository.ClearSubstitute();
+        }
+
+        [Test]
+        public async Task Handle_WhenRequestIsValid_ShouldReturnResults()
+        {
+            // Arrange
+            var hotels = GenerateHotels(10);
+            var request = new FindHotelByNameRequest("Search_Term", 1, 10);
+            var queryResponse = new QueryResponse<Hotel>
+            (
+                request.PageNumber,
+                request.PageSize,
+                hotels.Count,
+                hotels
+            );
+
+            _mockHotelRepository.PerformQueryAsync
+            (
+                Arg.Any<QueryRequest<Hotel>>(),
+                Arg.Any<CancellationToken>()
+            ).Returns(queryResponse);
+
+            // Act
+            var result = await _handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            result.Data.ShouldBeEquivalentTo(queryResponse);
+        }
+
+        private static List<Hotel> GenerateHotels(int howMany)
+        {
+            var hotelFaker = new HotelFaker(1297);
+            return hotelFaker.Generate(howMany);
+        }
+    }
+}
+#nullable enable
